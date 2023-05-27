de21767dd7b2878e1cbfa97b54ec4bc7c5aaf9f8
Balogh Gabriel Claudiu(Outland3r2007) <baloggabriel76@outlook.com>
2023-05-27 09:26:34 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -6,7 +6,6 @@ var apps = [
 //     icon: "icon", 
 //     description: "description", 
 //     compatible: "compatible", 
-//     developer: "developer", 
 //     get_link: "get_link", 
 //     type: "type", 
 //     screenshots: [
@@ -18,9 +17,8 @@ var apps = [
       title: "iPASource",
       subtitle: "3rd Party App Store",
       icon: "ipasource/assets/favicon.ico",
-      description: 'Best iPA Library.<br>Powered by <a style="color:grey;text-decoration:underline;" href="https://tweakrain.tk">tweakra1n</a>',
+      description: 'Best iPA Library.',
       compatible: "N/A",
-      developer: "@Outland3r2007",
       get_link: "ipasource/home.html",
       type: "Website",
       screenshots: [
@@ -35,7 +33,6 @@ var apps = [
       icon: "assets/blacklist.png",
       description: "If you're blacklisted and you can't install 3rd Party<br>Apps use this app,it works on iOS",
       compatible: "iOS 14-15.7.1 and 16.0-16.1.2",
-      developer: "@AppInstalleriOS",
       get_link: "https://appinstallerios.com/TrollStoreIPAs/Blacklist.ipa",
       type: "App/iPA",
       screenshots: [
@@ -50,7 +47,6 @@ var apps = [
       icon: "assets/appbox.png",
       description: "Find apps within minutes without ads, paywalls or subscriptions.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://appbox.click",
       type: "Website",
       screenshots: [
@@ -65,7 +61,6 @@ var apps = [
       icon: "assets/default.png",
       description: "Browse our library of iPAs, with the option of on-device install<br>NOTE: all iPAs on Zeus.me do NOT violate any copyright law. Everything on our site is 100% legal to download.",
       compatible: "N/A",
-      developer: "@zeusdotme",
       get_link: "https://zeus.me",
       type: "Website",
       screenshots: [
@@ -80,7 +75,6 @@ var apps = [
       icon: "https://www.eonhubapp.com/assets/icon.png",
       description: "Simple, Easy, EonHub.<br>One of the best 3rd Party App Stores with Blacklist protection.",
       compatible: "N/A",
-      developer: "@DevCasp",
       get_link: "https://eonhubapp.com",
       type: "Website",
       screenshots: [
@@ -96,7 +90,6 @@ var apps = [
       icon: "https://acutus.fun/assets/img/icon.png",
       description: "EonHub mirror with less ads.",
       compatible: "N/A",
-      developer: "@DevCasp",
       get_link: "https://acutus.fun",
       type: "Website",
       screenshots: [
@@ -111,7 +104,6 @@ var apps = [
       icon: "https://pbs.twimg.com/profile_images/1627714387061374994/b5b9sJk0_400x400.jpg",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://ithunder.co",
       type: "App",
       screenshots: [
@@ -126,7 +118,6 @@ var apps = [
       icon: "https://eclipseemu.me/play/static/icons/apple-touch-icon.png",
       description: "The unerevokable multi-emulator for iOS.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://eclipseemu.me/play",
       type: "Website",
       screenshots: [
@@ -141,7 +132,6 @@ var apps = [
       icon: "assets/dzmoha.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "@contact_nadhir",
       get_link: "https://dzmohaipa.com",
       type: "Website",
       screenshots: [
@@ -155,7 +145,6 @@ var apps = [
       icon: "assets/apphub.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://apphub.name",
       type: "Website",
       screenshots: [
@@ -169,7 +158,6 @@ var apps = [
       icon: "https://chromahue.dev/assets/img/icon.png",
       description: "Chroma Hue lets you create custom wallpapers with a beautiful rgb halo effect and now lets you share your own wallpapers.<br>Chroma Hue is an app that allows you to quickly create custom wallpapers. The wallpapers have a gradient border around them with the colors of your choice. You can also add your own custom images to the wallpapers. I hope you like it, Enjoy!",
       compatible: "iOS 15.0 or later",
-      developer: "@DevCasp",
       get_link: "https://apps.apple.com/ro/app/chroma-hue/id6444013215",
       type: "App",
       screenshots: [
@@ -185,7 +173,6 @@ var apps = [
       icon: "assets/showae.png",
       description: "Theme your Homescreen icons and make your Homescreen amazing with our super easy to use Icon theming app! ShowAE has over 300 unique icon effects so you can theme your icons and have an amazing looking Homescreen!<br>Choose from multiple icon effect layers to create a unique and amazing icon effect.<br>Export a single image to share or automatically apply to your favourite icons. It’s so simple too!",
       compatible: "iOS 16.0 or later",
-      developer: "@SeanKly",
       get_link: "https://apps.apple.com/ro/app/showae/id6444632255",
       type: "App",
       screenshots: [
@@ -202,7 +189,6 @@ var apps = [
       icon: "assets/screenshotsl.png",
       description: "Make unique mockups for your screenshots.",
       compatible: "iOS 15.0 or later",
-      developer: "@SeanKly",
       get_link: "https://apps.apple.com/ro/app/screenshotsl/id1658857348",
       type: "App",
       screenshots: [
@@ -217,7 +203,6 @@ var apps = [
       icon: "https://pbs.twimg.com/profile_images/1642469735701905411/s54t5lx3_400x400.jpg",
       description: "No description available.",
       compatible: "N/A",
-      developer: "@MartipaApp",
       get_link: "https://martipa.co",
       type: "Website",
       screenshots: [
@@ -231,7 +216,6 @@ var apps = [
       icon: "assets/show.png",
       description: "Customize your homescreen and lock screen like never before.",
       compatible: "iOS 15.0 or later",
-      developer: "@SeanKly",
       get_link: "https://apps.apple.com/ro/app/showcuts/id1615409976",
       type: "App",
       screenshots: [
@@ -246,7 +230,6 @@ var apps = [
       icon: "assets/morfic.png",
       description: "Create beautiful wallpapers easily.",
       compatible: "iOS 15.0 or later",
-      developer: "@darkhueapp",
       get_link: "https://apps.apple.com/ro/app/morfic-live-dynamic-wallpaper/id1663728720",
       type: "App",
       screenshots: [
@@ -261,7 +244,6 @@ var apps = [
       icon: "assets/trollhub.png",
       description: "iPA Library for TrollStore",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://previewer.adalo.com/61e9a709-935d-4a4b-b745-8137b8994296/?source=pwa",
       type: "Website",
       screenshots: [
@@ -276,7 +258,6 @@ var apps = [
       icon: "https://raw.githubusercontent.com/Ibinou/-new-NoCodeHubInstaller/main/img/FD19EBF5-0177-462C-8C10-9C5B37766B29.png",
       description: "No description available",
       compatible: "N/A",
-      developer: "@Ibatech1",
       get_link: "https://nocodehub.ga",
       type: "Website",
       screenshots: [
@@ -291,7 +272,6 @@ var apps = [
       icon: "assets/iwebappstore.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "@enzo_lzrt",
       get_link: "https://enzo-zsh.github.io/iWebAppStore/",
       type: "Website",
       screenshots: [
@@ -306,7 +286,6 @@ var apps = [
       icon: "https://pbs.twimg.com/profile_images/1367920028813787144/cwvAQV9f_400x400.jpg",
       description: "Welcome to Scarlet, the fastest and smoothest experience to install and manage your favorite apps. Scarlet has a range of options the most popular being direct install or installing with the Scarlet computer method.",
       compatible: "N/A",
-      developer: "@GetScarlet",
       get_link: "https://usescarlet.com",
       type: "App",
       screenshots: [
@@ -321,7 +300,6 @@ var apps = [
       icon: "assets/zeus.png",
       description: "Zeus is the leading free signing service for iOS. We provide a top quality service for you to get your favorite tweaked apps and legacy jailbreaks.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://getzeus.app/app/index.html",
       type: "App",
       screenshots: [
@@ -336,7 +314,6 @@ var apps = [
       icon: "assets/altstore.png",
       description: "A home for apps that push the boundaries of iOS. No jailbreak required.",
       compatible: "N/A",
-      developer: "@rileytestut",
       get_link: "get_link",
       type: "App",
       screenshots: [
@@ -351,7 +328,6 @@ var apps = [
       icon: "https://app.topipas.com/img/TopIPAsLogov2.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.topipas.com",
       type: "Website",
       screenshots: [
@@ -366,7 +342,6 @@ var apps = [
       icon: "https://www.postbox.news/assets/images/appIconSite.webp",
       description: "PostBox brings the latest jailbreak news to your fingertips. Use PostBox to access your favorite packages and repositories while unjailbroken, all without the hassle of constantly searching on the web. With in-app .deb download and extraction, you can install themes using only your phone.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://postbox.news",
       type: "App",
       screenshots: [
@@ -381,7 +356,6 @@ var apps = [
       icon: "assets/default.png",
       description: "The biggest wallpapers community,share your wallpapers,comment.4K quality wallpapers for free.",
       compatible: "N/A",
-      developer: "@Outland3r2007",
       get_link: "https://photos.google.com/share/AF1QipOi51vtjEyORw_3h6MS6CD8EZx7X959iNPiWYMiLjScughelvQ8iL60PW9g9ErnSw?key=S0ttQW9OMUtBU19oMGNEcVpRSjQtY21hQjRCWmRn",
       type: "Website",
       screenshots: [
@@ -395,7 +369,6 @@ var apps = [
       icon: "assets/broank.png",
       description: "Customizing has no limits.",
       compatible: "N/A",
-      developer: "@Polyphian",
       get_link: "https://broank.com",
       type: "Website",
       screenshots: [
@@ -409,7 +382,6 @@ var apps = [
       icon: "https://www.drag0nos.ga/Img/Drag0nOS.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://drag0nos.ga",
       type: "Website",
       screenshots: [
@@ -424,7 +396,6 @@ var apps = [
       icon: "https://jailbreaks.app/img/Jailbreaks.png",
       description: "Jailbreaks app store",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://jailbreaks.app",
       type: "Website",
       screenshots: [
@@ -439,7 +410,6 @@ var apps = [
       icon: "https://app.airport.community/logo2.svg",
       description: "Airport is the best place to discover new TestFlight apps from developers.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.airport.community",
       type: "Website",
       screenshots: [
@@ -454,7 +424,6 @@ var apps = [
       icon: "https://bunnyinject.fun/img/bunnyinjecticon.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.airport.community",
       type: "Website",
       screenshots: [
@@ -469,7 +438,6 @@ var apps = [
       icon: "https://app.iosgods.com/apple-touch-icon.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.iosgods.com/store/",
       type: "Website",
       screenshots: [
@@ -484,7 +452,6 @@ var apps = [
       icon: "https://www.cokernutx.com/coker/img/icon.png",
       description: "CokernutX is a new integrated application download platform, including IOS, Android, IOS jailbreak, MAC and other platforms. CokernutX will always be Free and there are over 1 Million users worldwide.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://www.cokernutx.com/mobileIndexWeb.php",
       type: "Website",
       screenshots: [
@@ -499,7 +466,6 @@ var apps = [
       icon: "https://app.ipabox.store/uploads/icon/ErQqYeXL5117ZtXrV15mrsfWwNm5P88WZH4I5CMS.png",
       description: "iPABox is an app installer where you can get some of the hacks, emulators, ++tweaks that allows you to unlock many features.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.ipabox.store/store",
       type: "Website",
       screenshots: [
@@ -514,7 +480,6 @@ var apps = [
       icon: "https://is.gd/2EiU1B",
       description: "Tweakdoor is an Alternative App Store for iPhone & iPad.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.tweakdoor.com",
       type: "Website",
       screenshots: [
@@ -529,7 +494,6 @@ var apps = [
       icon: "assets/tubemax.png",
       description: "YouTube Premium for free.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://apps.apple.com/ro/app/tubemax-videos-music-streams/id1634335563",
       type: "Website",
       screenshots: [
@@ -544,7 +508,6 @@ var apps = [
       icon: "https://sidestore.io/apple-touch-icon.0a9d800f.png",
       description: "AltStore fork that does not require AltServer.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://sidestore.io",
       type: "Website",
       screenshots: [
@@ -559,7 +522,6 @@ var apps = [
       icon: "https://www.kurdstore.info/assets/icon.png",
       description: "No description available.",
       compatible: "N/A",
-      developer: "N/A",
       get_link: "https://app.kurdstore.info",
       type: "Website",
       screenshots: [
@@ -568,3 +530,60 @@ var apps = [
       badge: ""
   }
 ]
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(registration => {
+     
+      console.log('Service Worker registered!');
+
+      var toastSuccess = app.toast.create({
+        text: '<i style="font-size:16px;color:lightgreen;" class="f7-icons ">checkmark_alt_circle_fill</i> Service Worker registered successfully.',
+        closeTimeout: 2000
+      });
+      toastSuccess.open();
+    })
+    .catch(error => {
+     
+      console.error('<i style="font-size:16px;color:red;" class="f7-icons color-red">xmark_circle_fill</i> Service Worker registration failed:', error);
+
+      var toastError = app.toast.create({
+        text: 'Service Worker registration failed.',
+        closeTimeout: 2000
+      });
+      toastError.open();
+    });
+}
+
+function update() {
+  
+  app.dialog.confirm('This will re-register the service-worker and update all the files.', 'Refresh app files?', function() {
+   
+    var preloaderDialog = app.dialog.preloader('Refreshing files...');
+    preloaderDialog.open();
+
+    setTimeout(function() {
+      preloaderDialog.close();
+      window.location.reload(); 
+    }, 3000);
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        registrations.forEach(registration => {
+          registration.unregister();
+        });
+      }).then(() => {
+         navigator.serviceWorker.register('service-worker.js')
+          .then(registration => {
+            
+            console.log('Service worker re-registered:', registration);
+
+            registration.update();
+          })
+          .catch(error => {
+            
+            console.error('Service worker registration failed:', error);
+          });
+      });
+    }
+  });
+}
--- service-worker.js
+++ service-worker.js
@@ -5,26 +5,18 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll([
         '/',
-        'index.html',
-        'home.html',
+        'app.html',
         'css/f7.css',
         'css/styles.css',
         'js/app.js',
         'js/nobounce.js',
         'js/routes.js',
-        './data.js'
+        'assets/default.png',
+        './data.js',
         'https://cdn.jsdelivr.net/npm/framework7@7/framework7-bundle.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.compat.css',
-        'assets/favicon.ico'
+        'assets/favicon.ico',
         'manifest.json',
-        'manifest2.json',
-        'ipasource/home.html',
-        'ipasource/search.html',
-        'ipasource/settings.html',
-        'ipasource/terms.html',
-        'ipasource/start.html',
-        'ipasource/css/styles.css',
-        'ipasource/manifest.json',
+        'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
       ]))
   );
 });
